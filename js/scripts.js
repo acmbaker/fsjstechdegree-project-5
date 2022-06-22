87742f73aa6ae7bbffc8b2d7285867ba4a5a88ce
acmbaker <104640776+acmbaker@users.noreply.github.com>
2022-06-22 17:20:00 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 // Fetch API
 let dataOriginal;
-fetch('https://randomuser.me/api/?results=12')
-    .then(data => data.json())
-    .then(data => dataOriginal = data.results)
-    .then(data => gallery(dataOriginal));
+fetch("https://randomuser.me/api/?results=12")
+  .then((data) => data.json())
+  .then((data) => (dataOriginal = data.results))
+  .then((data) => gallery(dataOriginal));
 
 // Search Container
 document.querySelector("div.search-container").insertAdjacentHTML(
   "beforeend",
-    `
+  `
     <form action="#" method="get">
     <input type="search" id="search-input" class="search-input" placeholder="Search...">
     <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
@@ -18,11 +18,11 @@ document.querySelector("div.search-container").insertAdjacentHTML(
 
 // Gallery Function
 function gallery(data) {
-    document.querySelector("div#gallery").innerHTML = '';
-    for (let i = 0; i < data.length; i++) {
-        document.querySelector("div#gallery").insertAdjacentHTML(
-            "beforeend",
-            `
+  document.querySelector("div#gallery").innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    document.querySelector("div#gallery").insertAdjacentHTML(
+      "beforeend",
+      `
             <div class="card">
                 <div class="card-img-container">
                     <img class="card-img" src="${data[i].picture.thumbnail}" alt="profile picture">
@@ -34,70 +34,98 @@ function gallery(data) {
                 </div>
             </div>
             `
-        );
-    }
-    eventListeners(data);
+    );
+  }
+  eventListeners(data);
 }
 
+// Modal popup and switch buttons
 function eventListeners(data) {
-    let allCards = document.querySelectorAll('div.card');
-    for (let i = 0; i < allCards.length; i++) {
-        allCards[i].addEventListener('click', (e) => {
-            modalPopup(data[i]);
-            switchButtons(data, i);
-        });
-    }
-
-    document.querySelector('input#search-input').addEventListener('keyup', (e) => {
-        let updatedData = [];
-        for (let j = 0; j < dataOriginal.length; j++) {
-            if (dataOriginal[j].name.first.toUpperCase().includes(e.target.value.toUpperCase()) || dataOriginal[j].name.last.toUpperCase().includes(e.target.value.toUpperCase())) {
-                updatedData.push(dataOriginal[j]);
-            }
-        }
-        gallery(updatedData);
+  let allCards = document.querySelectorAll("div.card");
+  for (let i = 0; i < allCards.length; i++) {
+    allCards[i].addEventListener("click", (e) => {
+      modalPopup(data[i]);
+      switchButtons(data, i);
     });
+  }
 }
 
+// Search filter functionality
+document.querySelector("input#search-input").addEventListener("keyup", (e) => {
+  let updatedData = [];
+  for (let j = 0; j < dataOriginal.length; j++) {
+    if (
+      dataOriginal[j].name.first
+        .toUpperCase()
+        .includes(e.target.value.toUpperCase()) ||
+      dataOriginal[j].name.last
+        .toUpperCase()
+        .includes(e.target.value.toUpperCase())
+    ) {
+      updatedData.push(dataOriginal[j]);
+    }
+  }
+  gallery(updatedData);
+});
+
+// Prev and Next button functionaliy
 function switchButtons(data, position) {
-    document.querySelector("div.modal-container").insertAdjacentHTML(
-        "beforeend", `
+  document.querySelector("div.modal-container").insertAdjacentHTML(
+    "beforeend",
+    `
     <div class="modal-btn-container">
         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
         <button type="button" id="modal-next" class="modal-next btn">Next</button>
     </div>
-    `)
+    `
+  );
 
-    document.querySelector('button.modal-close-btn').addEventListener('click', (e) => { document.querySelector('div.modal-container').remove()});
-
-    document.querySelector('button.modal-prev').addEventListener('click', (e) => {
-        document.querySelector('div.modal-container').remove();
-        position -= 1;
-        if (data.length > position && position !== -1) {
-            modalPopup(data[position]);
-            switchButtons(data, position);
-        }
+  // Remove modal popup once close button clicked
+  document
+    .querySelector("button.modal-close-btn")
+    .addEventListener("click", (e) => {
+      document.querySelector("div.modal-container").remove();
     });
 
-    document.querySelector('button.modal-next').addEventListener('click', (e) => {
-        document.querySelector('div.modal-container').remove();
-        position += 1;
-        if (data.length > position) {
-            modalPopup(data[position]);
-            switchButtons(data, position);
-        }
-    });
+  // For when the previous button is clicked
+  document.querySelector("button.modal-prev").addEventListener("click", (e) => {
+    document.querySelector("div.modal-container").remove();
+    position -= 1;
+    if (data.length > position && position !== -1) {
+      modalPopup(data[position]);
+      switchButtons(data, position);
+    }
+  });
+
+  // For when the next button is clicked
+  document.querySelector("button.modal-next").addEventListener("click", (e) => {
+    document.querySelector("div.modal-container").remove();
+    position += 1;
+    if (data.length > position) {
+      modalPopup(data[position]);
+      switchButtons(data, position);
+    }
+  });
 }
 
+// Popup display formatting
 function modalPopup(data) {
-    let dobSplit = data.dob.date.slice(0, 10).split('-');
-    let dob = `${dobSplit[1]}/${dobSplit[2]}/${dobSplit[0]}`;
-    let phoneSplit = data.phone.toString().replaceAll(' ','').replaceAll('(', '').replaceAll(')','').replaceAll('-', '');
-    let phone = `(${phoneSplit.substring(0,3)}) ${phoneSplit.substring(3,6)}-${phoneSplit.substring(6)}`;
+  let dobSplit = data.dob.date.slice(0, 10).split("-");
+  let dob = `${dobSplit[1]}/${dobSplit[2]}/${dobSplit[0]}`;
+  let phoneSplit = data.phone
+    .toString()
+    .replaceAll(" ", "")
+    .replaceAll("(", "")
+    .replaceAll(")", "")
+    .replaceAll("-", "");
+  let phone = `(${phoneSplit.substring(0, 3)}) ${phoneSplit.substring(
+    3,
+    6
+  )}-${phoneSplit.substring(6)}`;
 
-        document.querySelector("body").insertAdjacentHTML(
-            "beforeend",
-            `
+  document.querySelector("body").insertAdjacentHTML(
+    "beforeend",
+    `
             <div class="modal-container">
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
@@ -114,5 +142,5 @@ function modalPopup(data) {
                 </div>
             </div>
             `
-        );
+  );
 }
